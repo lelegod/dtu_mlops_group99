e44901e181e7689e477ec730f648163c232a65a1
lelegod <2021m622@kuas.ac.jp>
2026-01-09 10:35:07 +0100

    create python project

---
+++ project99/src/project99/train.py
@@ -0,0 +1,10 @@
+from project99.model import Model
+from project99.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
